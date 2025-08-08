897ebfbf4daececb75cf3802f4a2a994801743e4
Keerthana6525 <keerthana.ammu6525@gmail.com>
2025-08-08 16:56:58 +0530

    Add root URL view for Rendertest

--- Backend/backend_project/urls.py
+++ Backend/backend_project/urls.py
@@ -18,9 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Django backend is running on Render!")
 
 urlpatterns = [
+    path('',home),
     path('admin/', admin.site.urls),
     path('core/', include('core.urls')),
 ]
